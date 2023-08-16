b27cf6dc082ae138b6a48e7f9c77ef29035899d2
qiang <star.lvqiang@gmail.com>
2023-08-16 12:21:23 +0800

    [qiang]: add revice message to parse and call service and init message format

--- src/RpcProvider.cc
+++ src/RpcProvider.cc
@@ -5,6 +5,7 @@
 
 #include "RpcProvider.h"
 #include "MprpcApplication.h"
+#include "client_request.pb.h"
 
 void RpcProvider::NotifyService(::google::protobuf::Service *service)
 {
@@ -47,8 +48,89 @@ void RpcProvider::Run()
 
 void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr &conn)
 {
+    if (!conn->connected())
+    {
+        // rpc client断开了
+        conn->shutdown();
+    }
 }
 
-void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr &, muduo::net::Buffer *, muduo::Timestamp)
+void RpcProvider::OnMessage(const muduo::net::TcpConnectionPtr &conn, muduo::net::Buffer *buffer, muduo::Timestamp timestamp)
 {
+    // 接收client的请求信息
+    std::string recv_buf = buffer->retrieveAllAsString();
+    std::cout << "receive message: " << recv_buf << std::endl;
+
+    uint32_t header_size = 0;
+    recv_buf.copy((char *)&header_size, 4, 0);
+
+    std::string rpc_header_str = recv_buf.substr(4, header_size);
+    mprpc::ClientRequest client_requst;
+    std::string service_name;
+    std::string method_name;
+    uint32_t args_size;
+    if (client_requst.ParseFromString(rpc_header_str))
+    {
+        service_name = client_requst.service_name();
+        method_name = client_requst.method_name();
+        args_size = client_requst.args_size();
+    }
+    else
+    {
+        std::cout << "rpc header str: " << rpc_header_str << " parse error!" << std::endl;
+        return;
+    }
+    std::string args_str = recv_buf.substr(4 + header_size, args_size);
+
+    std::cout << "======================================" << std::endl;
+    std::cout << "header size: " << header_size << std::endl;
+    std::cout << "rpc header str: " << rpc_header_str << std::endl;
+    std::cout << "service name: " << service_name << std::endl;
+    std::cout << "method name: " << method_name << std::endl;
+    std::cout << "args size: " << args_size << std::endl;
+    std::cout << "args str: " << args_str << std::endl;
+    std::cout << "======================================" << std::endl;
+
+    auto it = serivce_map.find(service_name);
+    if (it == serivce_map.end())
+    {
+        std::cout << service_name << " is not exist!" << std::endl;
+        return;
+    }
+    google::protobuf::Service *service = it->second.service;
+    auto method_it = it->second.provider_method_map.find(method_name);
+    if (method_it == it->second.provider_method_map.end())
+    {
+        std::cout << service_name << ":" << method_name << " is not exist!" << std::endl;
+        return;
+    }
+    const google::protobuf::MethodDescriptor *method = method_it->second;
+
+    google::protobuf::Message *request = service->GetRequestPrototype(method).New();
+    if (!request->ParseFromString(args_str))
+    {
+        std::cout << "request parse error, content:" << args_str << std::endl;
+        return;
+    }
+    google::protobuf::Message *response = service->GetResponsePrototype(method).New();
+
+    // call method最后一个参数 自定义函数 要绑定一个Closure函数
+    google::protobuf::Closure *done =
+        google::protobuf::NewCallback<RpcProvider, const muduo::net::TcpConnectionPtr &, google::protobuf::Message *>(this, &RpcProvider::SendRpcResponse, conn, response);
+
+    service->CallMethod(method, nullptr, request, response, done);
+}
+
+void RpcProvider::SendRpcResponse(const muduo::net::TcpConnectionPtr &conn, google::protobuf::Message *response)
+{
+    std::string response_str;
+    if (response->SerializeToString(&response_str))
+    {
+        conn->send(response_str);
+    }
+    else 
+    {
+        std::cout << "serializa response str to string ffailed!" << std::endl;
+    }
+    conn->shutdown();
 }
